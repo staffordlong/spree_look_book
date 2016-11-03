7b9ff1162f4fccb0a97fbec679d2a1a99504313a
askl56 <amscott@openmailbox.org>
2016-11-03 16:18:10 +0000

    Rename controller and add helper

--- app/helpers/spree/look_book_helper.rb
+++ app/helpers/spree/look_book_helper.rb
@@ -1,2 +1,8 @@
-module Spree::LookBookHelper
+# frozen_string_literal: true
+module Spree
+  module LookBookHelper
+    def find_product_by_id(product)
+      Spree::Product.find_by(id: product.spree_product_id)
+    end
+  end
 end
