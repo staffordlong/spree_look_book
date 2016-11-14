fe2bcfe74cf83a31fd486b623ccc96d4f0249c6f
askl56 <amscott@openmailbox.org>
2016-11-14 11:33:54 +0000

    Switch routes from add_routes to the more conventional routes.draw method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :look_books do
       resources :look_book_images, only: %i(index create destroy new edit update) do
