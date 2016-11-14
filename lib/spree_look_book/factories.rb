546048d894777e816af3615d2fd28118a200c077
askl56 <amscott@openmailbox.org>
2016-11-14 11:15:48 +0000

    Move factories to lib folder

--- lib/spree_look_book/factories.rb
+++ lib/spree_look_book/factories.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'spree_look_book/factories'
+  sequence(:lookbook_name) { |n| "Lookbook - #{n}" }
+
+  factory :look_book, class: 'Spree::LookBook' do
+    title              { FFaker::Lorem.sentence(1) }
+    slug               { title }
+    active             { true }
+  end
 end
