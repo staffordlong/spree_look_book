8f3ca240969e7173c3a9d760042cdbd46cb596be
askl56 <amscott@openmailbox.org>
2016-11-14 11:32:58 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
-Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+Spree::Core::Engine.add_routes do
+  namespace :admin do
+    resources :look_books do
+      resources :look_book_images, only: %i(index create destroy new edit update) do
+        post :update_positions, on: :collection
+        resources :look_book_image_products do
+          collection do
+            post :update_positions
+          end
+        end
+      end
+    end
+
+  scope 'look-books' do
+    get '/:slug', to: 'look_books#show',  as: :look_book
+  end
 end
